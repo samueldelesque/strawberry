e70023435c2a64c19db8b185412cebbf774b063c
samueldelesque <contact@samueldelesque.com>
2015-02-24 22:39:52 -0500

    removed app basepath, changed some styling

--- client/app.js
+++ client/app.js
@@ -23,7 +23,15 @@ config(['$routeProvider', function($routeProvider) {
 		lookingForGender: ""
 	}
 
-	$scope.log = function(m){console.log(m)}
+	$scope.allResults = results_fixtures
+
+	var interests = []
+	angular.forEach($scope.allResults,function(e,i){
+		if(interests.indexOf(e.lookingFor) == -1) interests.push(e.lookingFor)
+	})
+	$scope.interests = interests
+
+	$scope.log = console.log
 	$scope.toggleMenu = function(){
 		$scope.body.toggleClass("show-nav")
 	}
@@ -83,3 +91,126 @@ config(['$routeProvider', function($routeProvider) {
 		}
    })
 });;
+
+
+var results_fixtures = [
+	{
+		name: "Henri",
+		gender: "m",
+		age: 60,
+		lookingFor: "lick pussy",
+		lookingForGender: "f"
+	},
+	{
+		name: "Margaret",
+		gender: "f",
+		age: 31,
+		lookingFor: "lick pussy",
+		lookingForGender: "f"
+	},
+	{
+		name: "Sam & Carlotta",
+		gender: "couple",
+		age: 24,
+		lookingFor: "lick a dick",
+		lookingForGender: "m"
+	},
+	{
+		name: "8th st wankers",
+		gender: "group",
+		age: 44,
+		lookingFor: "gang bang",
+		lookingForGender: "f"
+	},
+	{
+		name: "SexySushi",
+		gender: "group",
+		age: 24,
+		lookingFor: "gang bang",
+		lookingForGender: "any"
+	},
+	{
+		name: "Stephanie",
+		gender: "f",
+		age: 34,
+		lookingFor: "lick a dick",
+		lookingForGender: "m"
+	},
+	{
+		name: "Chey",
+		gender: "f",
+		age: 23,
+		lookingFor: "eat cheese",
+		lookingForGender: "friends"
+	},
+	{
+		name: "Sam",
+		gender: "m",
+		age: 23,
+		lookingFor: "drink a beer",
+		lookingForGender: "m"
+	},
+	{
+		name: "Matthieu",
+		gender: "m",
+		age: 23,
+		lookingFor: "drink a beer",
+		lookingForGender: "any"
+	},
+	{
+		name: "Carlotta",
+		gender: "f",
+		age: 23,
+		lookingFor: "lick pussy",
+		lookingForGender: "any"
+	},
+	{
+		name: "Jeremy",
+		gender: "m",
+		age: 24,
+		lookingFor: "get married",
+		lookingForGender: "f"
+	},
+	{
+		name: "Robert",
+		gender: "m",
+		age: 24,
+		lookingFor: "eat cheese",
+		lookingForGender: "any"
+	},
+	{
+		name: "Kelsey",
+		gender: "f",
+		age: 26,
+		lookingFor: "eat cheese",
+		lookingForGender: "any"
+	},
+	{
+		name: "Josh",
+		gender: "m",
+		age: 23,
+		lookingFor: "eat tacos",
+		lookingForGender: "any"
+	},
+	{
+		name: "Gabriel",
+		gender: "m",
+		age: 30,
+		lookingFor: "drink a beer",
+		lookingForGender: "any"
+	},
+	{
+		name: "Brenna",
+		gender: "f",
+		age: 22,
+		lookingFor: "get whipped",
+		lookingForGender: "any"
+	},
+	{
+		name: "Angel",
+		gender: "m",
+		age: 90,
+		lookingFor: "get whipped",
+		lookingForGender: "m"
+	}
+]
